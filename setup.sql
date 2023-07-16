1ae3b8b18b4f2e0d747644b4683e68703c66a331
blck-snwmn <whitesnowmancreator@gmail.com>
2023-07-16 23:51:37 +0900

    Add order_histories table

--- setup.sql
+++ setup.sql
@@ -38,3 +38,38 @@ VALUES (1, 1, 'Apple', 10, '2023-01-01'),
        (11, 8, 'Honeydew', 110, '2023-11-01'),
        (12, 9, 'Ice Cream Bean', 120, '2023-12-01'),
        (13, 10, 'Jackfruit', 130, '2024-01-01');
+
+CREATE TYPE order_status AS ENUM ('order_accepted', 'paid', 'ordered', 'shipped', 'delivered');
+
+CREATE TABLE order_histories (
+    history_id SERIAL PRIMARY KEY,
+    order_id INT,
+    customer_id INT,
+    product_name VARCHAR(100),
+    order_date DATE,
+    status order_status,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+);
+
+INSERT INTO order_histories (order_id, customer_id, product_name, order_date, status)
+VALUES
+(1, 1, 'Product 1', '2022-01-01', 'order_accepted'),
+(1, 1, 'Product 1', '2022-01-01', 'paid'),
+(1, 1, 'Product 1', '2022-01-01', 'ordered'),
+(1, 1, 'Product 1', '2022-01-02', 'shipped'),
+(1, 1, 'Product 1', '2022-01-03', 'delivered'),
+(2, 2, 'Product 2', '2022-02-01', 'order_accepted'),
+(2, 2, 'Product 2', '2022-02-01', 'paid'),
+(2, 2, 'Product 2', '2022-02-01', 'ordered'),
+(2, 2, 'Product 2', '2022-02-02', 'shipped'),
+(2, 2, 'Product 2', '2022-02-03', 'delivered'),
+(3, 1, 'Product 3', '2022-03-01', 'order_accepted'),
+(3, 1, 'Product 3', '2022-03-01', 'paid'),
+(3, 1, 'Product 3', '2022-03-01', 'ordered'),
+(3, 1, 'Product 3', '2022-03-02', 'shipped'),
+(4, 2, 'Product 1', '2022-04-01', 'order_accepted'),
+(4, 2, 'Product 1', '2022-04-01', 'paid'),
+(4, 2, 'Product 1', '2022-04-01', 'ordered'),
+(4, 2, 'Product 1', '2022-04-02', 'shipped'),
+(5, 1, 'Product 2', '2022-05-01', 'order_accepted');
